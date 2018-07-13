4e760d69f9c4fe585cee23c05b191f7585a5a6ae
cMing <mayday404@163.com>
2018-07-13 19:20:28 +0800

    微信的加载方式优化

--- src/plugins/wechat/index.js
+++ src/plugins/wechat/index.js
@@ -1,6 +1,9 @@
 import wx from 'weixin-js-sdk'
 import {isWeChat} from '../fit-tool-lite'
-import loadWx from './loadWx'
+let cbList = []
+let loaded = false
+let firstEntry = true
+
 
 const _wx = new Proxy(wx, {
   get (target, name) {
@@ -8,7 +11,7 @@ const _wx = new Proxy(wx, {
       return function (){
         console.log('当前不是微信环境 跳过jsdk配置')
       }
-    } else if (name in target) {
+    } else if (loaded && name in target) {
       return wx[name]
     } else {
       return function (...arg) {
@@ -17,6 +20,54 @@ const _wx = new Proxy(wx, {
     }
   }
 })
+
+async function loadWx(callback = () => {}) {
+  if (!isWeChat()) {
+    return true
+  }
+  if (!firstEntry) {
+    cbList.push(callback)
+  } else {
+    firstEntry = false
+    let res = await requestJsbill()
+    wx.config({
+      debug: false,
+      appId: res.data.appId,
+      timestamp: res.data.timestamp,
+      nonceStr: res.data.nonceStr,
+      signature: res.data.signature,
+      jsApiList: [
+        'checkJsApi', 'openLocation', 'getLocation', 'onMenuShareTimeline', 'onMenuShareAppMessage', 'hideMenuItems', 'uploadImage', 'chooseImage', 'scanQRCode', 'chooseWXPay'
+      ]
+    })
+    await new Promise((resolve, reject) => {
+      wx.ready(() => resolve())
+      wx.error((err) => resolve(err))
+    })
+    loaded = true
+    callback()
+    if (cbList.length) {
+      cbList.forEach(cb => cb())
+      cbList = []
+    }
+  }
+}
+
+function requestJsbill () {
+  return new Promise((resolve, reject) => {
+    let xhr = new XMLHttpRequest()
+    xhr.open('GET', '/lens/fit/h5/api/v1/wechat/jsbill?type=1&url=' + encodeURIComponent(window.location.href))
+    xhr.send(null)
+    xhr.onreadystatechange = () => {
+      if (xhr.status === 200) {
+        if (xhr.readyState === 4) {
+          resolve(JSON.parse(xhr.responseText))
+        }
+      }
+    }
+  })
+}
+
 
 export default {
   install (Vue) {
